960f61721672e4792369cef585dedcee4a8248ea
exclusiveabhi <imexclusiveabhi@gmail.com>
2024-08-20 19:40:48 +0530

    20/08/2024

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Login from './auth/Login';
+import SignUp from "./auth/SignUp";
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-    
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/signin" />} />
+        <Route path="/signin" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<Navigate to="/notfound" />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
