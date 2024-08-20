db007aafd34ce2fb10bb89c1c1f34ecdcb62f527
exclusiveabhi <imexclusiveabhi@gmail.com>
2024-08-20 19:50:59 +0530

    20/08/2024

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,7 +8,6 @@ import SaveIcon from '@mui/icons-material/Save';
 import PrintIcon from '@mui/icons-material/Print';
 import ShareIcon from '@mui/icons-material/Share';
 
-
 const actions = [
   { icon: <FileCopyIcon />, name: 'Copy' },
   { icon: <SaveIcon />, name: 'Save' },
